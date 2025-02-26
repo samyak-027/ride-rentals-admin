a11e5af62cf7d47712456978bd878f8d5d4eb2af
Samyak Acharya <samyakacharya@MacBookAir.bbrouter>
2025-02-27 00:26:06 +0530

    Added Admin Name

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,28 +1,41 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Import for navigation
+import { useNavigate } from 'react-router-dom';
 import AdminNavbar from '../components/AdminNavbar';
 import CarCard from '../components/CarCard';
 import Loader from '../components/Loader';
 import Skeleton from '../components/Skeleton';
-import AddCarIcon from '../assets/add-car.svg'; // Import SVG
+import AddCarIcon from '../assets/add-car.svg';
 
 function Dashboard() {
+  const [adminName, setAdminName] = useState('');
   const [cars, setCars] = useState([]);
   const [loading, setLoading] = useState(true);
   const [skeleton, setSkeleton] = useState(true);
-  const navigate = useNavigate(); // Hook for navigation
+  const navigate = useNavigate();
+
+  // Fetch admin name
+  useEffect(() => {
+    fetch('http://localhost:5007/api/admin/profile', {
+      method: 'GET',
+      credentials: 'include', // Ensures cookies are sent
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          setAdminName(data.admin.name);
+        }
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
   useEffect(() => {
-    // Simulate skeleton state for 2 seconds
     const skeletonTimer = setTimeout(() => {
       setSkeleton(false);
     }, 2000);
 
-    // Fetch all cars from backend API
     fetch('http://localhost:5007/api/cars/getCars')
       .then((res) => res.json())
       .then((data) => {
-        // Simulate additional loader for 1 second
         setTimeout(() => {
           setCars(data);
           setLoading(false);
@@ -37,7 +50,6 @@ function Dashboard() {
   }, []);
 
   const handleDeleteCar = (carId) => {
-    // Call backend API to delete car and then update state
     fetch(`http://localhost:5007/api/cars/${carId}`, { method: 'DELETE' })
       .then((res) => res.json())
       .then(() => {
@@ -51,7 +63,11 @@ function Dashboard() {
       <AdminNavbar />
       <div className="p-4">
         <div className="flex justify-between items-center mb-4">
+        <div className="flex flex-col">
+          <h1 className="text-4xl font-extrabold">Hey, {adminName}👋</h1>
           <h1 className="text-2xl font-bold">All Cars</h1>
+        </div>
+
           <button
             onClick={() => navigate('/car-form')}
             className="flex items-center gap-2 bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-600"
