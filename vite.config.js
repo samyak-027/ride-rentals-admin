84dafacda253b6b052823b8cdcf45c3867fa878d
samyak-0027 <samyak.acharya119590@marwadiuniversity.ac.in>
2025-08-22 00:12:52 +0530

    for aws changed host server

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
   server: {
-    host: '0.0.0.0', // listen on all network interfaces
+    host: '0.0.0.0', // <- Allows access from external hosts (e.g., EC2 DNS)
     port: 5173,
-    allowedHosts: [
-      'ec2-50-16-20-9.compute-1.amazonaws.com' // ✅ allow your EC2 public DNS
-    ],
     proxy: {
       '/api': {
         target: 'http://localhost:5007',
